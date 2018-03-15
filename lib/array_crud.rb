e1f2b961c1aadcc1be1daa96b99e37c0119269f2
Nikki Apostolakis <nikki.apostolakis@gmail.com>
2018-03-15 02:47:24 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -5,12 +5,15 @@ def create_an_empty_array
 end
 
 def create_an_array
+
   myarray = [1, 2, 3, 4]
 
 end
 
 def add_element_to_end_of_array(array, element)
 
+  array << element
+  
 end
 
 def add_element_to_start_of_array(array, element)
